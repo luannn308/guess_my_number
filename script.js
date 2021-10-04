e2d5c44b0237d108b8d2b11da6cf5afc9cf741e9
Luan Nguyen <luannn308@gmail.com>
2021-10-04 19:09:27 +0700

    Upload Source Code

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+let ranNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+function checkNumber() {
+  let guess = Number(document.querySelector('.guess').value);
+  if (!guess)
+    document.querySelector('.message').textContent = '❌ No Number !!!';
+  else if (guess < 1 || guess > 20)
+    document.querySelector('.message').textContent =
+      '❗ Fail ! between 1 and 20 ';
+  else {
+    if (ranNumber === guess) {
+      document.querySelector('.message').textContent = '🎉Correct Number!!! ';
+      document.querySelector('.number').textContent = ranNumber;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+      if (highscore < score) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    } else if (ranNumber < guess) {
+      document.querySelector('.message').textContent = '📈 TOO HIGH';
+      score !== 0 ? score-- : (score = 0);
+      document.querySelector('.score').textContent = score;
+      if (score === 0) {
+        document.querySelector('.message').textContent = '🏳 YOU LOSE!!! ';
+      }
+    } else {
+      document.querySelector('.message').textContent = '📉 TOO LOW';
+      score !== 0 ? score-- : (score = 0);
+      document.querySelector('.score').textContent = score;
+      if (score === 0) {
+        document.querySelector('.message').textContent = '🏳 YOU LOSE!!! ';
+      }
+    }
+  }
+}
+document.querySelector('.check').addEventListener('click', checkNumber);
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  ranNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing... ';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = null;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
